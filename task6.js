f674aff094cb045f844b72dfbfc6790ce062c5ec
Oscar Chávez Vasquez <ochv.job@gmail.com>
2025-11-07 00:49:58 -0300

    Agregando mi solución para Task 6

--- task6.js
+++ task6.js
@@ -1,31 +1,41 @@
-import PromptSync from "prompt-sync"
+import promptSync from 'prompt-sync';
 
-const prompt = PromptSync();
+let resultados = [];
+const prompt = promptSync();
 
 const limite = parseInt(prompt("¿Hasta qué numero te muestro?: "));
 
-let resultados = [];
-
 if(limite<=105){
-  for (let i = 0; i <= limite; i++) {
+  for (let i = 1; i <= limite; i++) {
       if(i%3==0 && i%5==0 && i%7==0){
-        console.log("FizzBuzzWoof");
+        resultados.push("FizzBuzzWoof");
+
       }else if(i%3==0 && i%7==0){
-        console.log("FizzWoof");
+        resultados.push("FizzWoof");
+
       }else if(i%5==0 && i%7==0){
-        console.log("BuzzWoof");
+        resultados.push("BuzzWoof");
+
       }else if(i%3==0 && i%5==0){
-        console.log("FizzBuzz"); 
+        resultados.push("FizzBuzz"); 
+
       }else if(i%3==0){
-        console.log("Fizz");
+        resultados.push("Fizz");
+
       }else if(i%5==0){
-        console.log("Buzz");
+        resultados.push("Buzz");
+
       }else if(i%7==0){
-        console.log("Woof");
+        resultados.push("Woof");
+
       }else{ 
-        console.log(i);
+        resultados.push(i);
       }
     }
 }else{
   console.log("No puedes ingresar números mayores a 105!!");
 }
+for (let j = 0; j < resultados.length; j++) {
+  console.log(resultados[j]);
+}
+console.log("Total de elementos en lista:",resultados.length);
