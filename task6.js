5ae89981f76a6bb97cd80a902c9729dc9f28a231
Naomi Nuñez <lamimi.nunezibarra@gmail.com>
2025-11-09 19:19:55 -0300

    task6.js
    The program prompts the user for a number and prints 'Fizz', 'Buzz', 'Woof', or 'FizzBuzz' based on divisibility rules up to that number, with a limit of 105.

--- task6.js
+++ task6.js
@@ -1,5 +1,43 @@
-// Refer to Task 6 in your Instructions to complete this task
+import PromptSync from "prompt-sync"
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Six!");
-  };
+const prompt = PromptSync();
+
+const limite = parseInt(prompt("¿Hasta qué numero te muestro?: "));
+
+let resultados = [];
+
+if(limite<=105){
+  for (let i = 0; i <= limite; i++) {
+      if(i%3==0 && i%5==0 && i%7==0){
+        console.log("FizzBuzzWoof");
+      }else if(i%3==0 && i%7==0){
+        console.log("FizzWoof");
+      }else if(i%5==0 && i%7==0){
+        console.log("BuzzWoof");
+      }else if(i%3==0 && i%5==0){
+        console.log("FizzBuzz"); 
+      }else if(i%3==0){
+        console.log("Fizz");
+      }else if(i%5==0){
+        console.log("Buzz");
+      }else if(i%7==0){
+        console.log("Woof");
+      }else{ 
+        console.log(i);
+      }
+    }
+}else{
+  console.log("No puedes ingresar números mayores a 105!!");
+}
+:100:
+Haz clic para reaccionar
+:thumbsup:
+Haz clic para reaccionar
+:thumbsdown:
+Haz clic para reaccionar
+Agregar reacción
+Responder
+Reenviar
+Más
+
+Mensaje Trabajos grupales
